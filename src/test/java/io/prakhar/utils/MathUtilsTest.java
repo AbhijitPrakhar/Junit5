56bf673c6d6220838414df478c81ba750087e4f5
PRAKHAR ABHIJIT <praksabhijit@gmail.com>
2019-05-19 09:23:27 +0530

    Junit poc

---
+++ src/test/java/io/prakhar/utils/MathUtilsTest.java
@@ -0,0 +1,28 @@
+package io.prakhar.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
+
+    @Test
+    void computeCircleArea() {
+    }
+}
